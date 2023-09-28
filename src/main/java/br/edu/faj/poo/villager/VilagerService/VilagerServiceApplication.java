44de0c2c0e63e0571b056368e78d9d7f79264ecf
Rafael Menegon <dev.rafaelmenegon@gmail.com>
2023-09-28 20:12:35 -0300

    feat: initial commit

---
+++ src/main/java/br/edu/faj/poo/villager/VilagerService/VilagerServiceApplication.java
@@ -0,0 +1,13 @@
+package br.edu.faj.poo.villager.VilagerService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VilagerServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VilagerServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/edu/faj/poo/villager/VilagerService/VilagerServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package br.edu.faj.poo.villager.VilagerService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VilagerServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
